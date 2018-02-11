6a11a149d58858d8a7cb01d56462e5e26a22c24e
Caio Zanetoni <cmzanetoni@gmail.com>
2018-02-11 13:20:19 -0200

    Incluida classe Posicao

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using tabuleiro;
 
 namespace xadrez_console {
     class Program {
         static void Main(string[] args) {
+            Posicao posicao = new Posicao(2, 3);
+
+            Console.WriteLine(posicao);
+            Console.ReadLine();
         }
     }
 }
